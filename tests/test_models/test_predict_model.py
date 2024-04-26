5cbfa43fd29cc69d581d8b691c27cea609c121ae
chvalois <chvalois@yahoo.fr>
2024-04-26 17:20:28 +0200

    Added user_matrix sample as fixture for tests

--- tests/test_models/test_predict_model.py
+++ tests/test_models/test_predict_model.py
@@ -6,7 +6,7 @@ from models.predict_model import make_predictions
 def test_number_predictions():
 
     users_id = [1, 2]
-    matrix = make_predictions(users_id, "src/models/model.pkl", "src/data/processed/user_matrix.csv")
+    matrix = make_predictions(users_id, "src/models/model.pkl", "tests/fixtures/user_matrix_test.csv")
 
     assert matrix.shape == (2, 10)
 
